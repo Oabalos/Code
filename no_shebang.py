1b012d287cfb71ee406ef5d428bf367131cce7b9
oabalos <oabalos@github.com>
2020-03-30 18:23:40 -0700

    mix-list/

---
+++ no_shebang.py
@@ -0,0 +1,4 @@
+print("ello!")
+print("Did you say, hello?")
+print("no, i said ello, but that\'s close enough.")
+
---
+++ with_shebang.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+print("ello!")
+print("DId you say, hello?")
+print("no, I said ello, but that/'s close enough.")
+
